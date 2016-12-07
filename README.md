31ef0ebadb32dd2b006c13acc2380dd39749bcb8
Aymeric Flaisler <aflaisler@gmail.com>
2016-12-07 16:59:34 +0000

    added upload to gd

---
+++ README.md
@@ -0,0 +1,2 @@
+# affiliate_networks_data
+Retrieve, merge and clean data from the different affiliate networks
---
+++ upload_gdrive.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+# import json
+from df2gspread import df2gspread as d2g
+
+
+def upload_to_drive(df):
+    spreadsheet = '1gAUh4rXrD5vlVodmmC5wARmIKWssJOxMWbTGQzs-FlE'
+    # staging: spreadsheet = '1ZDXIEn6CQMEZmbPsQ0GsDpu4oS65RUjZtp2dFwExdDA'
+    wks_name = 'GMV_affiliate_3m'
+    print('uploading')
+    d2g.upload(df, gfile=spreadsheet, wks_name=wks_name, clean=True, df_size=True)
+
+    
+if __name__ == '__main__':
+    df = pd.read_csv('gmv_affiliates.csv')
+    df = df.fillna('').reset_index(drop=True)
+    upload_to_drive(df)
